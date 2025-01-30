11a465e089b8836d96b5a398f881d684d16d36da
yashBhende1 <yash.bhende24@pcu.edu.in>
2025-01-30 15:59:18 +0530

    Update app.js

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -8,7 +8,9 @@ app.get('/', function (req, res) {
 app.get('/mars', function(req, res) {
   res.send('Hello Mars!\n');
 });
-
+app.get('/data',()=>{
+  res.send("whether api")
+})
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
